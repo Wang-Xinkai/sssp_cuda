992df4d4bb51433c44747ca00a68c3d9b67320f5
王鑫凯 <772918068@qq.com>
2020-05-11 23:06:02 +0800

    adjust file

--- temp/shortest_path.h
+++
@@ -1,3 +0,0 @@
-
-void shortestPath_Dijkstra(int num_node, int *vex, float *arc,int v0, int *path_node, float *shortLenTable);
-void shortestPath_floyd(int num_node, int *vex, float *arc, int *path_node, float *shortLenTable);
